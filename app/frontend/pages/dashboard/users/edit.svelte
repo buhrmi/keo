a3f94ea7c510764482f4f8637b028fad7b3601bd
Stefan Buhrmester <stefan@buhrmi.de>
2026-05-31 16:18:35 +0700

    show header/profile image

--- app/frontend/pages/dashboard/users/edit.svelte
+++ app/frontend/pages/dashboard/users/edit.svelte
@@ -7,13 +7,18 @@
 </svelte:head>
 
 <main>
-  <section>
-    <div class="header_image full w-full">
-    {JSON.stringify(current_user)}
-      
-    </div>
-    <div class="flex">
-      <div class="avatar"></div>
+  <section class="bg-black">
+    <div
+      class="header_image full w-full"
+      style={current_user.header_image ? `background-image: url('${current_user.header_image.url}')` : ''}
+    ></div>
+
+    
+    <div class="profile_content flex full px-4">
+      <div class="rounded-top"></div>
+      {#if current_user.profile_image}
+        <img src={current_user.profile_image.url} alt="Profile Image" class="avatar" />
+      {/if}
       <div class="ml-4">
         <h1 class="text-2xl font-bold">{current_user.name}</h1>
         <p class="text-gray-500">@{current_user.username}</p>
@@ -25,10 +30,32 @@
 
 <style>
   .header_image {
+    position: sticky;
+    top: 0;
+    z-index: 0;
     background-position: center;
+    background-repeat: no-repeat;
     background-size: cover;
     background-color: #222;
     height: 200px;
+  }
+
+  .profile_content {
+    position: relative;
+    z-index: 1;
+    background: var(--bg-primary);
+  }
+
+  .rounded-top {
+    position: absolute;
+    z-index: -1;
+    top: -1rem;
+    left: 0;
+    right: 0;
+    height: 1rem;
+    background: var(--bg-primary);
+    border-top-left-radius: 9999px;
+    border-top-right-radius: 9999px;
   }
 
   .avatar {
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,14 @@ u = User.find_or_create_by!(id: 1) do |user|
   user.email = "stefan@keoscout.com"
   user.password = SecureRandom.base58(10)
   user.name = "Da Nang Bass Man"
+  user.username = "danangbassman"
 end
 
 u.identities.find_or_create_by!(provider: "zalo", provider_id: "206492388818353401")
+
+# p = u.posts.find_or_create_by!(id: 1) do |post|
+#   p.text = "Drum and Bass live from Da Nang, Vietnam. Catch me on Twitch: https://www.twitch.tv/danangbassman"
+# end
 
 u.profile_image.attach(
   io: File.open(Rails.root.join("db/seeds/profile.png")),
